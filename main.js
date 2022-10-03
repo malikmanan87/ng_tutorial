7aeffe2266b5bb31b8e29d46d3e1c4c4fe5730ae
malik <mrabdmalik@gmail.com>
2022-10-03 15:23:19 +0800

    kickstarter

---
+++ main.js
@@ -0,0 +1,5 @@
+function log(message) { //declaration function
+    console.log(message); //method
+}
+var message = 'hello world'; //input
+log(message); //action
---
+++ main.ts
@@ -0,0 +1,5 @@
+function log(message){
+    console.log();    
+}
+var message = 'hello world';
+log(message);
