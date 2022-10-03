8dff2eadeb2737f773aa4ca9d8562f43a81f477c
malik <mrabdmalik@gmail.com>
2022-10-03 16:01:38 +0800

    extend code

--- main.js
+++
@@ -1,5 +0,0 @@
-function log(message) { //declaration function
-    console.log(message); //method
-}
-var message = 'hello world'; //input
-log(message); //action
--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
-function log(message){
-    console.log();    
+function log(message) {  //declaration function
+  console.log(message); //method
 }
-var message = 'hello world';
-log(message);
+var message = "hello world"; //input
+log(message); //action
+
+// variable declaration info, var -> for global/function, let -> for local bracket/block
+
+function doSomething() {
+  for (var i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  console.log("Finally: " + i);
+}
+
+doSomething();
+
+let count;
+count = "a";
+log(count); //action
